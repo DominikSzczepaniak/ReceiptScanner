29d5007bd86481e7d714b603554379ad5e5abc3c
DominikSzczepaniak <szczepaniakdominik5@gmail.com>
2024-07-08 19:55:04 +0200

    Tailwindcss setup and some data and loading images for pytesseract

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      
+      <div className="bg-red-500">
+        <p className="text-3xl font-bold underline">Hello</p>
+      </div>
     </>
   )
 }
